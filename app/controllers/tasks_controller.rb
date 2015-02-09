57f0a95acb40dc2634b2b8a5c6661bb2ae1a2f29
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-08 18:33:44 -0700

    Added create action in Tasks controller, changed formatting on Tasks index view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,21 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
+
 end
