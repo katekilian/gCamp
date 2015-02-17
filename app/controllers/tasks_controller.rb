7fa564b20e7eda36019db6bd44aab129926f4b8c
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-17 15:51:33 -0700

    Added Due Date field in Index view, got due dates to display

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:description, :completed)
+    params.require(:task).permit(:description, :completed, :due_date)
   end
 
 end
