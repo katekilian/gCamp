b5ee7462ad76dc6017fe27d9ce852dd5778d39c1
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-18 16:05:21 -0700

    Created Show action/view for Users, put links to Show in Users Index view

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   resources :tasks
 
-  resources :users, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 end
