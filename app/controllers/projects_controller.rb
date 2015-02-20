e549c64e3e73d7b5d6fb82e87d7c04dbc8112cf7
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-20 15:07:39 -0700

    Added Projects New view, filled in with form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,25 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
 end
