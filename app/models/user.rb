929ca666450d04cfd8aca452081acf4115800e33
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-03-05 21:22:11 -0700

    Added error messages to Registrations New view, styled with Bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  has_secure_password
 
 end
