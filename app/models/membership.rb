b0401600348b15c7758c3a3ae27e5c6b49549be2
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-03-11 22:30:30 -0600

    Created Role table, model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,6 @@ class Membership < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :project
+  belongs_to :role
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311032624) do
+ActiveRecord::Schema.define(version: 20150312042021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20150311032624) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tasks", force: :cascade do |t|
