3695119ed87df1bb3f030cc3471e1bff16750e49
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-03-16 16:51:38 -0600

    Added validation for uniqueness for users per membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,6 +4,6 @@ class Membership < ActiveRecord::Base
   belongs_to :project
   belongs_to :role
 
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :project_id, message: "has already been added to this project" }
 
 end
