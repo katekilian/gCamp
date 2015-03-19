1de60423eb843be43b2dfa4a00404d3995edaf62
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-03-18 23:15:55 -0600

    Added count of all activity on gCamp to About Index view

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,11 @@
 class AboutController < ApplicationController
 
   def index
+    @projects = Project.all
+    @tasks = Task.all
+    @project_members = Membership.all
+    @users = User.all
+    @comments = Comment.all
   end
 
 end
