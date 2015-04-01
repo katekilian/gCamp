e630c9a02af23f9890145b54ac10b0e5074e035e
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-04-01 16:16:25 -0600

    Added token attribute to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331204835) do
+ActiveRecord::Schema.define(version: 20150401221236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20150331204835) do
     t.string  "email"
     t.string  "password_digest"
     t.boolean "admin",           default: false
+    t.string  "token"
   end
 
   add_foreign_key "memberships", "projects"
